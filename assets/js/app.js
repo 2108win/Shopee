69726607baab9fc6a8044923ec0517d6bdace571
Win Lã <dangcapcuawin@gmail.com>
2022-08-20 01:44:32 +0700

    update something

---
+++ assets/js/app.js
@@ -0,0 +1,21 @@
+var headerNavNotifyListItem = document.getElementsByClassName("header-nav-notify-list-item");
+for (var i = 0; i < headerNavNotifyListItem.length; i++) {
+  headerNavNotifyListItem[i].addEventListener("click", function () {
+    this.classList.toggle("header-nav-notify-list-item-seen");
+  });
+}
+
+var shoppingcartCount = document.getElementById("shoppingcartCount");
+shoppingcartCount.innerHTML = document.getElementsByClassName(
+  "header-main-shoppingcart-list-item"
+).length;
+
+$(document).ready(function () {
+  $('.banner-main-slideshow').slick({
+    dots: true,
+    autoplay: true,
+    delay: 3500,
+  });
+});
+
+$('.single-item').slick();
